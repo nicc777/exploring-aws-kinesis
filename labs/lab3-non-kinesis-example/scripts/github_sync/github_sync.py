682f94a74867259925046c4f076e2fb2a2cd97ee
Nico Coetzee <nicc777@gmail.com>
2022-09-10 14:37:14 +0200

    feature: started working on the python script for the GitHub sync server

--- labs/lab3-non-kinesis-example/scripts/github_sync/github_sync.py
+++ labs/lab3-non-kinesis-example/scripts/github_sync/github_sync.py
@@ -1,17 +1,29 @@
 import logging
 import logging.handlers
 import time
+from datetime import datetime
 
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
+ch = logging.TimedRotatingFileHandler('/data/logs/github_sync.log', when='midnight', interval=1, backupCount=0, encoding=None, delay=False, utc=True, atTime=None, errors=None)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s:%(lineno)d - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+logger.info('Logging Configured')
+logger.debug('Debug Enabled')
 
-handler = logging.handlers.SysLogHandler(address='localhost')
 
-logger.addHandler(handler)
+def get_utc_timestamp(with_decimal: bool=False): # pragma: no cover
+    epoch = datetime(1970,1,1,0,0,0) 
+    now = datetime.utcnow() 
+    timestamp = (now - epoch).total_seconds() 
+    if with_decimal: 
+        return timestamp 
+    return int(timestamp) 
 
-logger.debug('this is debug')
-logger.critical('this is critical')
+
 
 while True:
     logger.info('MAIN LOOP')
