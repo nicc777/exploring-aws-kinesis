ccf65735c9fe34fae125095075b3d05f340bbe97
Nico Coetzee <nicc777@gmail.com>
2022-11-20 09:17:48 +0200

    lab4: transaction test data

--- labs/lab4-athena-query-s3-events/tests/prepare_test_events.py
+++ labs/lab4-athena-query-s3-events/tests/prepare_test_events.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 import boto3
+import copy
 
 
 file_name = None
@@ -36,3 +37,33 @@ print('Test Data contains {} records/transaction events'.format(len(test_data)-1
 
 
 
+
+
+def build_cash_deposit_event(data: dict)->dict:
+    event_data = dict()
+    print('Preparing a cash deposit event for source account {}'.format(data['Reference Account']))
+
+    return event_data
+
+
+def build_incoming_payment_event(data: dict)->dict:
+    event_data = dict()
+    print('Preparing an incoming payment event for source account {}'.format(data['Reference Account']))
+
+    return event_data
+
+
+
+def upload_event(event_data: dict)->bool:
+    
+    return True
+
+
+EVENT_BUILD_MAPPING = {
+    'cash_deposit_': build_cash_deposit_event,
+    'incoming_payment_': build_incoming_payment_event,
+}
+
+
+for event_record in test_data:
+    EVENT_BUILD_MAPPING[event_record['Transaction Type']](data=copy.deepcopy(event_record))
