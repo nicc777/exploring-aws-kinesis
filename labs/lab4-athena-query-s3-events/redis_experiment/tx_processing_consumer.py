9146fdb6b57d301b664e9211a08793ae3e25a850
Nico Coetzee <nicc777@gmail.com>
2022-11-30 08:41:57 +0200

    lab4: redis local experiment

---
+++ labs/lab4-athena-query-s3-events/redis_experiment/tx_processing_consumer.py
@@ -0,0 +1,136 @@
+import boto3
+import traceback
+import os
+import json
+import logging
+from datetime import datetime
+import sys
+from inspect import getframeinfo, stack
+# Other imports here...
+
+
+def get_logger(level=logging.INFO):
+    logger = logging.getLogger()
+    for h in logger.handlers:
+        logger.removeHandler(h)
+    formatter = logging.Formatter('%(funcName)s:%(lineno)d -  %(levelname)s - %(message)s')
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(level)    
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    logger.setLevel(level)
+    return logger
+
+
+def get_client(client_name: str, region: str='eu-central-1', boto3_clazz=boto3):
+    return boto3_clazz.client(client_name, region_name=region)
+
+
+# ADD the header as per section ``Module header functions``
+
+CACHE_TTL_DEFAULT = 600
+cache = dict()
+
+def get_utc_timestamp(with_decimal: bool = False):
+    epoch = datetime(1970, 1, 1, 0, 0, 0)
+    now = datetime.utcnow()
+    timestamp = (now - epoch).total_seconds()
+    if with_decimal:
+        return timestamp
+    return int(timestamp)
+    
+    
+def get_debug()->bool:
+    try:
+        return bool(int(os.getenv('DEBUG', '0')))
+    except:
+        pass
+    return False
+    
+
+def get_cache_ttl(logger=get_logger())->int:
+    try:
+        return int(os.getenv('CACHE_TTL', '{}'.format(CACHE_TTL_DEFAULT)))
+    except:
+        logger.error('EXCEPTION: {}'.format(traceback.format_exc()))
+    return CACHE_TTL_DEFAULT
+
+
+def refresh_environment_cache(logger=get_logger()):
+    global cache
+    now = get_utc_timestamp(with_decimal=False)
+    if 'Environment' in cache:
+        if cache['Environment']['Expiry'] > now:
+            return
+    cache['Environment'] = {
+        'Expiry': get_utc_timestamp() + get_cache_ttl(logger=logger),
+        'Data': {
+            'CACHE_TTL': get_cache_ttl(logger=logger),
+            'DEBUG': get_debug(),
+            # Other ENVIRONMENT variables can be added here... The environment will be re-read after the CACHE_TTL 
+        }
+    }
+    logger.debug('cache: {}'.format((json.dumps(cache))))
+
+
+def debug_log(message: str, variables_as_dict: dict=dict(), variable_as_list: list=list(), logger=get_logger(level=logging.INFO)):
+    if cache['Environment']['Data']['DEBUG'] is True:
+        try:
+            caller = getframeinfo(stack()[1][0])
+            caller_str = '{}():{}'.format(caller.function, caller.lineno)
+            message = '[{}]  {}'.format(caller_str, message)
+            if len(variables_as_dict) > 0:
+                logger.debug(message.format(**variables_as_dict))
+            else:
+                logger.debug(message.format(*variable_as_list))
+        except:
+            pass
+
+
+###############################################################################
+###                                                                         ###
+###                         M A I N    H A N D L E R                        ###
+###                                                                         ###
+###############################################################################
+
+    
+def handler(
+    event,
+    context,
+    logger=get_logger(level=logging.INFO),
+    boto3_clazz=boto3,
+    run_from_main: bool=False
+):
+    refresh_environment_cache(logger=logger)
+    if cache['Environment']['Data']['DEBUG'] is True and run_from_main is False:
+        logger  = get_logger(level=logging.DEBUG)
+    
+    debug_log('event={}', variable_as_list=[event], logger=logger)
+    
+    return {"Result": "Ok", "Message": None}    # Adapt to suite the use case....
+
+
+###############################################################################
+###                                                                         ###
+###                        M A I N    F U N C T I O N                       ###
+###                                                                         ###
+###############################################################################
+
+
+if __name__ == '__main__':
+    logger = logging.getLogger("my_lambda")
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(funcName)s:%(lineno)d -  %(levelname)s - %(message)s')
+
+    ch = logging.StreamHandler()
+    if get_debug() is True:
+        ch.setLevel(logging.DEBUG)    
+    else:
+        ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+    
+    if get_debug() is True:
+        logger.setLevel(logging.DEBUG)
+    else:    
+        logger.setLevel(logging.INFO)
+    handler(event={}, context=None, logger=logger, run_from_main=True)
